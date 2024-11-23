ae45b3f2a4ed9fabae02722d6a769d708f1beecb
dengyongcai <qq@example.com>
2024-11-24 00:47:36 +0800

    [fix]新增多语言

---
+++ utils/i18n/i18n_wrapper_test.go
@@ -0,0 +1,106 @@
+package i18n
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+	"sync"
+	"testing"
+)
+
+func TestTranslator_Translate(t *testing.T) {
+	translator, err := NewTranslator("./")
+	if err != nil {
+		t.Logf("err:%v\n", err)
+		return
+	}
+	SetDefaultTranslator(translator)
+	{
+		msg := Translate("zh", "100001")
+		log.Printf("msg:%v\n", msg)
+	}
+	{
+		msg := Translate("zh", "100002")
+		log.Printf("msg:%v\n", msg)
+	}
+
+}
+func TestTranslator_ConnTranslate(t *testing.T) {
+	// 假设 Translator 实例
+	translator1, _ := NewTranslator("./")
+	translator2, _ := NewTranslator("./")
+
+	// 初始化默认 Translator
+	SetDefaultTranslator(translator1)
+
+	// 并发测试
+	const numGoroutines = 1000
+	const iterations = 1000
+
+	var wg sync.WaitGroup
+	wg.Add(2 * numGoroutines)
+
+	// 并发执行 Translate
+	for i := 0; i < numGoroutines; i++ {
+		go func(id int) {
+			defer wg.Done()
+			for j := 0; j < iterations; j++ {
+				_ = Translate("en", "999999") // 使用默认的 msgId
+			}
+		}(i)
+	}
+
+	// 并发执行 SetDefaultTranslator
+	for i := 0; i < numGoroutines; i++ {
+		go func(id int) {
+			defer wg.Done()
+			for j := 0; j < iterations; j++ {
+				if j%2 == 0 {
+					SetDefaultTranslator(translator1)
+				} else {
+					SetDefaultTranslator(translator2)
+				}
+			}
+		}(i)
+	}
+
+	// 等待所有 Goroutine 完成
+	wg.Wait()
+
+	fmt.Println("Concurrent test completed")
+
+}
+func TestGin(t *testing.T) {
+	e := gin.New()
+	translator, _ := NewTranslator("./")
+	SetDefaultTranslator(translator)
+	e.GET("/getUserInfo", func(c *gin.Context) {
+		lang := c.Query("lang")
+		userId := c.Query("userId")
+
+		if userId == "1" {
+			c.JSON(200, gin.H{
+				"code": 200,
+				"msg":  "",
+				"data": gin.H{"username": "张三"},
+			})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"code": 500,
+			"msg":  Translate(lang, "100001"),
+		})
+	})
+	e.POST("/updateLangInfo", func(c *gin.Context) {
+		_ = os.WriteFile("./fr-FR.toml", []byte(`100001="Utilisateur introuvable"`), 0644)
+		t1, _ := NewTranslator("./")
+		SetDefaultTranslator(t1)
+		c.JSON(200, gin.H{
+			"code": 200,
+			"msg":  "",
+		})
+	})
+	e.Run(":8080")
+}
